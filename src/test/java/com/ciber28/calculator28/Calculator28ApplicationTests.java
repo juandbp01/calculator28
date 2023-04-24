d489292e827b44cb97d20576756ef37b50d2567f
Ubuntu <administrador@vmpeps-juandbp.mz5hyddagbsexi3ycfsaiele1a.rx.internal.cloudapp.net>
2023-04-24 14:33:09 +0000

    Añadido esqueleto Spring Boot

---
+++ src/test/java/com/ciber28/calculator28/Calculator28ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber28.calculator28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
